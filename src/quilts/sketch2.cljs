fdf657d9ea5273bfae6fd28b886875245f70e657
Erik <erik.svedang@gmail.com>
2014-11-25 15:47:30 +0100

    starfields!

--- src/quilts/sketch2.cljs
+++ src/quilts/sketch2.cljs
@@ -2,22 +2,28 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(def TWO-PI (* 2 3.14))
+
 (defn create-ship []
   {:pos [30 100]
    :dir 0.0
-   :speed 0.0})
+   :speed 0.0
+   :z 1.0})
 
 (defn create-star [pos]
   {:pos pos
-   :dir 0.0})
+   :dir (rand TWO-PI)
+   :size (+ 5.0 (rand 20.0))
+   :z (+ 0.5 (rand 1.0))})
+
+(defn random-star []
+  (create-star [(rand-int 200) (rand-int 200)]))
 
 (defn setup []
   (q/rect-mode :center)
   (q/frame-rate 30)
   {:ship (create-ship)
-   :stars [(create-star [50 50])
-           (create-star [60 50])
-           (create-star [70 50])]})
+   :stars (take 10 (repeatedly random-star))})
 
 (defn translate-v2 [[x y] [dx dy]]
   [(+ x dx) (+ y dy)])
@@ -50,22 +56,29 @@
 
 (defn draw-entity [entity [cam-x cam-y] render-fn]
   (let [[x y] (:pos entity render-fn)
-        dir (:dir entity render-fn)]
+        dir (:dir entity render-fn)
+        z (:z entity)]
     (q/fill 0 0 0)
     (do (q/push-matrix)
-        (q/translate (- x cam-x) (- y cam-y))
+        (q/translate (- x (* z cam-x)) (- y (* z cam-y)))
         (q/rotate dir)
-        (render-fn)
+        (render-fn entity)
         (q/pop-matrix))))
+
+(defn star-render [star]
+  (let [size (:size star)]
+    (q/fill 255)
+    (q/rect 0 0 size size)))
 
 (defn draw [state]
   (q/background 50 100 140)
   (let [ship-pos (-> state :ship :pos)
         cam-pos (translate-v2 ship-pos [(- (/ (q/width) 2))
                                         (- (/ (q/height) 2))])]
-    (draw-entity (:ship state) cam-pos #(q/rect 0 0 20 10))
+    (draw-entity (:ship state) cam-pos (fn [self] (q/rect 0 0 20 10)))
+    (q/no-stroke)
     (doseq [star (:stars state)]
-      (draw-entity star cam-pos (fn [] (q/rect 0 0 3 3))))))
+      (draw-entity star cam-pos star-render))))
 
 (defn run-sketch-2 []
   (q/defsketch quilts
